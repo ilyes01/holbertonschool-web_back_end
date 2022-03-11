8b80d21720f8c9883665496d3e7ffac3dde19de5
ilyes01 <ilyesghrab@gmail.com>
2022-03-11 14:00:35 -0800

    fiixxx

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" function element_length """
+""" a type-annotated function element_length """
 from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-"""element_lenght"""
+    """ element_length """
     return [(i, len(i)) for i in lst]
