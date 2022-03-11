5ccc3e1d1d03e746c20928df73024eabb2d8659d
ilyes01 <ilyesghrab@gmail.com>
2022-03-11 14:32:57 -0800

    update

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" task wait random """
+""" asynchronous coroutine """
 
 import asyncio
 import random
@@ -10,10 +10,10 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    async routine  task_wait_n
-    takes in 2 int arguments:
+    async routine called task_wait_n
+    that takes in 2 int arguments:
     max_delay and n.
-    wait_random n times with the specified max_delay.
+    You will spawn wait_random n times with the specified max_delay.
     """
     fcts = [
         task_wait_random(max_delay) for i in range(n)
