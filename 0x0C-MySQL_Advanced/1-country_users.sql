31663150cb1ec62ab3407d6a19a6ff38875f4b61
ilyes01 <ilyesghrab7@gmail.com>
2022-03-16 12:59:19 +0100

    first element

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+--SQL script that creat a table users 
+--script should not fail if the table already exists
+CREATE TABLE if NOT EXISTS users (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(255) NOT NULL UNIQUE,
+name VARCHAR(255),
+country ENUM("US", "CO", "TN") DEFAULT "US" NOT NULL
+);
+
