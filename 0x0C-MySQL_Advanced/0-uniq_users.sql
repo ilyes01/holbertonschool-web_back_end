6a381cc58d1fa0eb82bbf7d44c1fdd92d0b5f202
ilyes01 <ilyesghrab7@gmail.com>
2022-03-16 13:00:12 +0100

    update

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 --SQL script that creat a table users 
 --script should not fail if the table already exists
 CREATE TABLE if NOT EXISTS users (
-id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 email VARCHAR(255) NOT NULL UNIQUE,
 name VARCHAR(255)
 );
