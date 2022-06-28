77b40bc13dd1a7c3532043f2031b8f3ab3307878
ilyes01 <ilyesghrab7@gmail.com>
2022-06-28 15:28:12 +0200

    change

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    /* disable */
     const task = true;
     const task2 = false;
+    /* enable */
   }
 
   return [task, task2];
