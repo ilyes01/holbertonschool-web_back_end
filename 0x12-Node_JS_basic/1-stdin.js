050132505194c5a85c33ff68bf5d226aa4af0a81
ilyes01 <ilyesghrab7@gmail.com>
2022-07-11 12:04:16 +0200

    update

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,12 +1,10 @@
-console.log('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (n) => {
-  if (n.toString().trim() !== '') {
-    process.stdout.write(`Your name is: ${n.toString()}`);
-  } else {
-    process.exit();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const inpt = process.stdin.read();
+  if (inpt !== "") {
+    process.stdout.write(`Your name is: ${inpt}`);
   }
 });
-process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
-  process.exit();
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
 });
