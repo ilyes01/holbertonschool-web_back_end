9a2e8d74ee063ae042bbc8a39db166245183447e
ilyes01 <ilyesghrab7@gmail.com>
2023-01-17 11:01:22 +0100

    task

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,12 +1,8 @@
-console.log("Welcome to Holberton School, what is your name?");
-process.stdin.on('data', (n) => {
-  if (n.toString().trim() !== '') {
-    process.stdout.write(`Your name is: ${n.toString()}`);
-  } else {
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+    process.stdout.write('This important software is now closing\n');
     process.exit();
-  }
 });
-process.stdin.on('end', () => {
-  console.log('This important software is now closing');
-  process.exit();
-});
+
