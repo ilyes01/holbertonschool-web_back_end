ce0243733043ccb1525945f8aeb52fa514b730f2
ilyes01 <ilyesghrab7@gmail.com>
2023-01-23 14:28:40 +0100

    say maaah name

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,8 +1,18 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.question('', (name) => {
+  console.log(`Your name is: ${name}`);
+  rl.close();
+  console.log("This important software is now closing");
+});
+
+rl.on("close", function() {
+    process.exit(0);
+});
