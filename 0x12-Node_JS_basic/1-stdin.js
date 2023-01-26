b15ed015dee87effb62d33cf2287b69440ed3460
ilyes01 <ilyesghrab7@gmail.com>
2023-01-26 13:22:21 +0100

    lll

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -2,11 +2,12 @@
 var readline = require('readline');
 
 var rl = readline.createInterface(
-        process.stdin, process.stdout);
+      process.stdin, process.stdout);
 
-rl.setPrompt(`What is your name?\n`);
+rl.setPrompt(`Welcome to Holberton School, what is your name?\n`);
 rl.prompt();
 rl.on('line', (name) => {
-    console.log(`Your name is: ${name}`);
-    rl.close();
+  console.log(`Your name is: ${name}`);
+  rl.close();
+  console.log(`This important software is now closing\n`);
 });
