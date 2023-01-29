d6e31bb8bda018b2f6229b45f83e7834d68c1b03
ilyes01 <ilyesghrab7@gmail.com>
2023-01-29 14:32:13 +0100

    flask add

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,8 +1,10 @@
-
-from flask import Flask
+#!/usr/bin/env python3
+"""a flask app"""
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    """ render a html file """
     return render_template("0-index.html")
