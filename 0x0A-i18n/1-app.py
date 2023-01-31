3806670099a6779583194991c7c995489db7cabd
ilyes01 <ilyesghrab7@gmail.com>
2023-01-31 10:36:31 +0100

    tfghj

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 """a flask app"""
-
-
 from flask import Flask, render_template
 from flask_babel import Babel
 app = Flask(__name__)
 
-class Config:
+
+class Config(object):
     """configclass"""
     LANGUAGES = ['en','fr']
     BABEL_DEFAULT_TIMEZONE = 'UTC'
     BABEL_DEFAULT_LOCAL = 'en'
 
-app.config.from_object("1-app.Config")
+
+app.config.from_object("Config")
 babel = Babel(app)
+
 
 @app.route('/')
 def hello():
