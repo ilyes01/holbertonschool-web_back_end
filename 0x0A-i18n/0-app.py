d551146230c354831c5d6483d2e4b15a85c91d4b
ilyes01 <ilyesghrab7@gmail.com>
2023-01-31 09:30:46 +0100

    confic app flask

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -6,7 +6,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+LANGUAGE_CODE = 'en-us'
 
+TIME_ZONE = 'UTC'
+
+local = 'en'
 @app.route('/')
 def hello():
     """ render a html file """
