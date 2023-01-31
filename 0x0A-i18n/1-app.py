7f3eb17ce5b8c3d1bd2bf4ae150e281d8f8544ad
ilyes01 <ilyesghrab7@gmail.com>
2023-01-31 10:21:41 +0100

    commmmon

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -7,10 +7,10 @@ from flask_babel import Babel
 app = Flask(__name__)
 
 class Config:
-"""configclass"""
-LANGUAGE_CODE = ['en','fr']
-TIME_ZONE = 'UTC'
-local = 'en'
+    """configclass"""
+    LANGUAGE_CODE = ['en','fr']
+    TIME_ZONE = 'UTC'
+    local = 'en'
 
 app.config.from_object(Config)
 babel = Babel(app)
