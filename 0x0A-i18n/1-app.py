fe8678dff47d3c85f2fac0e78bbafde8610dad24
ilyes01 <ilyesghrab7@gmail.com>
2023-01-31 10:19:18 +0100

    updateee

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -8,11 +8,14 @@ app = Flask(__name__)
 
 class Config:
 """configclass"""
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = ['en','fr']
 TIME_ZONE = 'UTC'
 local = 'en'
+
+app.config.from_object(Config)
+babel = Babel(app)
 
 @app.route('/')
 def hello():
     """ render a html file """
-    return render_template('0-index.html')
+    return render_template('1-index.html')
