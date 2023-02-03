07bc5a7c7a27ca6719e37bb7300be78d7c811eff
ilyes01 <ilyesghrab7@gmail.com>
2023-02-03 16:07:21 +0100

    ufghj

--- 0x0B_redis_basic/exercice.py
+++ 0x0B_redis_basic/exercice.py
@@ -1,48 +1,53 @@
 #!/usr/bin/env python3
-import functools
+import uuid
+import redis
+from typing import Optional, Callable, Union
 
 class Cache:
-    def store(self, value):
-        key = str(hash(value))
-        self._data[key] = value
-        return key
 
-    def get(self, key, fn=None):
-        if key not in self._data:
-            return None
-        data = self._data[key]
-        if fn:
-            return fn(data)
-        return data
+    def __init__(self);
+    """
+    Constructor for the Cache class.
+    Initializes a Redis connection and flushes any existing data.
+    """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
-    def get_str(self, key):
-        return self.get(key, fn=lambda d: d.decode("utf-8"))
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+    """
+    Store a value in Redis.
+    :param data: The value to be stored, can be a string, bytes, int, or float.
+    :return: A random key generated using uuid to be used to retrieve the data later.
+    """
+        random_id = str(uuid.uuid4())
+        self._redis.set(random_id, data)
+        return random_id
 
-    def get_int(self, key):
-        return self.get(key, fn=int)
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+    """
+    Get a value from Redis, with the ability to convert the value to the original data type if desired.
+    :param key: The key used to retrieve the value from Redis.
+    :param fn: Optional callable to be used to convert the data back to the original data type.
+    :return: The original data type of the value retrieved from Redis.
+    """
+    return fn(self._redis.get(key)) if fn else self._redis.get(key)
 
-    _data = {}
-import functools
+    def get_str(self, value: bytes) -> str:
+    """
+    Converts a byte string to a utf-8 encoded string.
+    :param value: The byte string to be converted.
+    :return: The converted utf-8 encoded string.
+    """
+    return value.decode("utf-8")
 
-class Cache:
-    def store(self, value):
-        key = str(hash(value))
-        self._data[key] = value
-        return key
-
-    def get(self, key, fn=None):
-        if key not in self._data:
-            return None
-        data = self._data[key]
-        if fn:
-            return fn(data)
-        return data
-
-    def get_str(self, key):
-        return self.get(key, fn=lambda d: d.decode("utf-8"))
-
-    def get_int(self, key):
-        return self.get(key, fn=int)
-
-    _data = {}
+    def get_int(self, value: bytes) -> str:
+    """
+    Converts a byte string to an integer.
+    :param value: The byte string to be converted.
+    :return: The converted integer.
+    """
+        return int.from_bytes(value, sys.byteorder)
 
