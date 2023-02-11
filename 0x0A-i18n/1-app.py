9ffb53b1bdcc5cebcc4a5c623277efe1bc79fc73
ilyes01 <ilyesghrab7@gmail.com>
2023-02-11 17:44:36 +0100

    update

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -1,22 +1,43 @@
 #!/usr/bin/env python3
-"""a flask app"""
-from flask import Flask, render_template
+""" flask app"""
+
+# Import the required modules from the Flask library
+from flask import Flask, render_template, request, g
+
+# Import the Babel module from the flask_babel library
 from flask_babel import Babel
-app = Flask(__name__)
 
+# Create a Flask application instance
+app = Flask(_name_)
 
+# Define a Config class for the Babel object
 class Config(object):
-    """configclass"""
-    LANGUAGES = ['en','fr']
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-    BABEL_DEFAULT_LOCAL = 'en'
+    """ Config class """
 
+    # List of supported languages
+    LANGUAGES = ["en", "fr"]
 
-app.config.from_object("Config")
+    # Default language
+    BABEL_DEFAULT_LOCALE = "en"
+
+    # Default timezone
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+# Apply the Config object as the configuration for the Flask application
+app.config.from_object(Config)
+
+# Create a Babel object and initialize it with the Flask application
 babel = Babel(app)
 
-
+# Define a route for the root URL ('/')
 @app.route('/')
-def hello():
-    """ render a html file """
+def welcome():
+    """ render for html file """
+
+    # Render the '1-index.html' template file
     return render_template('1-index.html')
+
+# Check if the script is run as the main program (as opposed to being imported as a module)
+if _name_ == '_main_':
+    # Run the Flask application
+    app.run()
