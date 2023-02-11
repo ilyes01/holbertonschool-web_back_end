a795b680d90cf05d42557c42f15d33b89a5799cb
ilyes01 <ilyesghrab7@gmail.com>
2023-02-11 18:24:55 +0100

    update

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -1,58 +1,59 @@
 #!/usr/bin/env python3
 """ flask app"""
 
-# Import the required modules from the Flask library
 from flask import Flask, render_template, request, g
-
-# Import the Babel module from the flask_babel library
 from flask_babel import Babel
 
-# Create a Flask application instance
+"""Create a Flask application instance"""
 app = Flask(__name__)
 
-# Define a Config class for the Babel object
+""" Define a Config class for the Babel object"""
 
 
 class Config(object):
     """ Config class """
 
-    # List of supported languages
+    """ List of supported languages"""
     LANGUAGES = ["en", "fr"]
 
-    # Default language
+    """ Default language"""
     BABEL_DEFAULT_LOCALE = "en"
 
-    # Default timezone
+    """ Default timezone"""
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-# Apply the Config object as the configuration for the Flask application
+
+""" Apply the Config object as the configuration for the Flask application"""
 
 
 app.config.from_object(Config)
 
-# Create a Babel object and initialize it with the Flask application
+""" Create a Babel object and initialize it with the Flask application"""
 babel = Babel(app)
 
-# Define a`get_locale`function to determine the best match between the client's
+"""get_locale function to determine the best match between the client's"""
 @babel.localeselector
 def get_locale():
     """Determines the best match between the client's
     languages and our supported languages"""
 
-    # Use request.accept_languages to get a list of the client's
+    """ Use request.accept_languages to get a list of the client's"""
     return request.accept_languages.best_matches(app.config['LANGUAGES'])
-# Define a route for the root URL ('/')
+
+
+""" Define a route for the root URL ('/')"""
 @app.route('/')
 def welcome():
     """ render a html file """
 
-    # Render the '1-index.html' template file
+    """Render the '1-index.html' template file"""
     return render_template('2-index.html')
 
-# Check if the script is run as the main program
+
+"""Check if the script is run as the main program"""
 
 
 if __name__ == '__main__':
     """main"""
-    # Run the Flask application
+    """ Run the Flask application"""
     app.run()
