1a6143eccbd01c19b487a61a6d6b423a0fec5dc3
ilyes01 <ilyesghrab7@gmail.com>
2023-02-12 17:17:05 +0100

    kyr

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -35,7 +35,7 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """ Use request.accept_languages to get a list of the client's"""
-    return request.accept_languages.best_matches(app.config.LANGUAGES)
+    return request.accept_languages.best_match(app.config.LANGUAGES)
 
 
 """ Define a route for the root URL ('/')"""
