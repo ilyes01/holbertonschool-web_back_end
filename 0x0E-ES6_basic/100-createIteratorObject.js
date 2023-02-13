c312c786586189d7e9af45b3a7306d743fda761f
ilyes01 <ilyesghrab7@gmail.com>
2023-02-13 15:15:52 +0100

    es6

--- 0x0E-ES6_basic/100-createIteratorObject.js
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -1,21 +1,11 @@
 export default function createIteratorObject(report) {
-    const departments = Object.keys(report);
-    let currentDepartment = 0;
-    let currentEmployee = 0;
-
     return {
-        next: function () {
-            const department = departments[currentDepartment];
-            if (!department) {
-                return { done: true };
+        [Symbol.iterator]: function* () {
+            for (const department in report) {
+                for (const employee of report[department]) {
+                    yield employee;
+                }
             }
-            const employees = report[department];
-            if (currentEmployee >= employees.length) {
-                currentEmployee = 0;
-                currentDepartment++;
-                return this.next();
-            }
-            return { value: employees[currentEmployee++], done: false };
         }
-    };
+    }
 }
