941b4d547a9ebb29e8349e1b5a1b7c0399880967
ilyes01 <ilyesghrab7@gmail.com>
2023-02-16 14:19:35 +0100

    update

--- 0x0F-ES6_promise/0-promise.js
+++ 0x0F-ES6_promise/0-promise.js
@@ -1,7 +1,14 @@
 export default function getResponseFromAPI() {
+  const successResponse = 'Success';
+  const errorResponse = new Error('API call failed');
+
   return new Promise((resolve, reject) => {
-    resolve();
-    reject();
+    const success = true;
+    if (success) {
+      resolve(successResponse);
+    } else {
+      reject(errorResponse);
+    }
   });
 }
 
