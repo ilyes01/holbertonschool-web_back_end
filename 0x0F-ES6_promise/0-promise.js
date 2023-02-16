022435502cc0cd4b76fc47b637e48f4bedd5b3ae
ilyes01 <ilyesghrab7@gmail.com>
2023-02-16 13:48:06 +0100

    update

--- 0x0F-ES6_promise/0-promise.js
+++ 0x0F-ES6_promise/0-promise.js
@@ -1,8 +1,7 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     resolve();
-    // Commented out the reject call as it is unreachable and unnecessary
-    // reject();
+    reject();
   });
 }
 
