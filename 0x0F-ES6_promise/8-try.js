26f198d53029279e407045847042e154d3275c7c
ilyes01 <ilyesghrab7@gmail.com>
2023-02-16 16:34:46 +0100

    update

--- 0x0F-ES6_promise/8-try.js
+++ 0x0F-ES6_promise/8-try.js
@@ -1,9 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-  return new Promise((resolve, reject) => {
-    if (denominator === 0) {
-      reject(new Error('cannot divide by 0'));
-    } else {
-      resolve(numerator / denominator);
-    }
-  });
+  if (denominator === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+  return numerator / denominator;
 }
