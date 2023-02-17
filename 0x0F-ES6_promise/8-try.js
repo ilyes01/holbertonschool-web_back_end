5b91d7ac6975eb5c8723e9f67e50e34f2400e7ee
ilyes01 <ilyesghrab7@gmail.com>
2023-02-17 15:45:28 +0100

    try

--- 0x0F-ES6_promise/8-try.js
+++ 0x0F-ES6_promise/8-try.js
@@ -1,9 +1,12 @@
+// This function takes a numerator and a denominator and returns their quotient.
+// If the denominator is 0, an error is thrown.
+
 export default function divideFunction(numerator, denominator) {
-  return new Promise((resolve, reject) => {
-    if (denominator === 0) {
-      reject(new Error('Cannot divide by zero'));
-    } else {
-      resolve(numerator / denominator);
-    }
-  });
+  // Check if the denominator is 0.
+  if (denominator === 0) {
+    // If the denominator is 0, throw an error with the message "cannot divide by 0".
+    throw Error('cannot divide by 0');
+  }
+  // If the denominator is not 0, return the quotient of the numerator and denominator.
+  return numerator / denominator;
 }
