3f4eb862ed5bd59a12be03fe8fb4c4f749e84a01
ilyes01 <ilyesghrab7@gmail.com>
2023-02-21 11:36:33 +0100

    update

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   constructor(sqft) {
     if (this instanceof Building === false) {
-      if (!this.evacuationWarningMessage) throw new Error('Class extending Building must override evacuationWarningMessage');
+      if (!this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
