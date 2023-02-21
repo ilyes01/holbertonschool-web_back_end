700efb74ba11e80227556db10842d7c943a1d4cb
ilyes01 <ilyesghrab7@gmail.com>
2023-02-21 11:22:58 +0100

    class named building

---
+++ 0x10-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+// Building class
+class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  // Getters
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Abstract method for evacuation warning message
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
