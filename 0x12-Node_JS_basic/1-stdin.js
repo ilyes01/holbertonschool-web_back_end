570c5bf5528d5aa15247c42d4c4dd6e3488a113f
ilyes01 <ilyesghrab7@gmail.com>
2023-02-25 17:18:12 +0100

    update

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,18 +1,20 @@
+// Import the built-in readline module
 const readline = require('readline');
 
-console.log('Welcome to Holberton School, what is your name?');
-
+// Create an interface to read from stdin
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-rl.question('', (name) => {
+// Display welcome message and prompt for name
+console.log('Welcome to Holberton School, what is your name?');
+rl.prompt();
+
+// Handle user input
+rl.on('line', (name) => {
+  // Display user's name and close the program
   console.log(`Your name is: ${name}`);
-  rl.close();
-});
-
-process.on('exit', () => {
   console.log('This important software is now closing');
+  process.exit(0);
 });
-
