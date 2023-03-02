6f3b62ffb4d97450d5704824e9f5e3e4eb19a4cb
ilyes01 <ilyesghrab7@gmail.com>
2023-03-02 14:30:19 +0100

    dvbn,

--- 0x11-ES6_data_manipulation/0-get_list_students.js
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,7 @@
-function getListStudents() {
-  const students = [
+export default function getListStudent() {
+  return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-
-  return (students);
 }
-
-export default getListStudents;
