2d35aa2108b24464fbde6d7fc3bdb3c90c428a25
ilyes01 <ilyesghrab7@gmail.com>
2023-03-07 14:43:59 +0100

    updaaate

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,68 +1,70 @@
 #!/usr/bin/env python3
+"""
+Exercise file
+"""
+from typing import Callable, Optional, Union
 import redis
-import functools
-import json
-from typing import Callable, Union
+import uuid
+import sys
 from functools import wraps
+
+
+def replay(fn):
+    store = redis.Redis()
+    count_key = fn.__qualname__
+    input_key = count_key + ":inputs"
+    output_key = count_key + ":outputs"
+    count = store.get(count_key).decode("utf-8")
+    print("{} was called {} times:".format(count_key, count))
+    inputs = store.lrange(input_key, 0, int(count)-1)
+    outputs = store.lrange(output_key, 0, int(count)-1)
+    for input, output in zip(inputs, outputs):
+        input = input.decode("utf-8")
+        output = output.decode("utf-8")
+        print("{}(*{}) -> {}".format(count_key, input, output))
+
+
+
+def count_calls(method: Callable) -> Callable:
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+def call_history(method: Callable) -> Callable:
+    key = method.__qualname__
+    input_key = key + ":inputs"
+    output_key = key + ":outputs"
+
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.rpush(input_key, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(data))
+        return data
+    return wrapper
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def count_calls(method: Callable) -> Callable:
-        key = method.__qualname__
-
-        @wraps(method)
-        def wrapper(self, *args, **kwargs):
-            self._redis.incr(key)
-            return method(self, *args, **kwargs)
-        return wrapper
-
-    def call_history(method: Callable) -> Callable:
-        key = method.__qualname__
-
-        @wraps(method)
-        def wrapper(self, *args, **kwargs):
-            inputs_key = f"{key}:inputs"
-            outputs_key = f"{key}:outputs"
-            self._redis.rpush(inputs_key, json.dumps(args))
-            output = method(self, *args, **kwargs)
-            self._redis.rpush(outputs_key, json.dumps(output))
-            return output
-        return wrapper
-
-    @count_calls
     @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        random_id = str(uuid.uuid4())
+        self._redis.set(random_id, data)
+        return random_id
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
-        data = self._redis.get(key)
-        if data is None:
-            return None
-        if fn is not None:
-            data = fn(data)
-        return data
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        return fn(self._redis.get(key)) if fn else self._redis.get(key)
 
-    def get_str(self, key: str) -> Union[str, None]:
-        return self.get(key, fn=lambda x: x.decode("utf-8"))
-
-    def get_int(self, key: str) -> Union[int, None]:
-        return self.get(key, fn=lambda x: int(x))
-
-    def replay(method: Callable):
-    key = method.__qualname__
-    input_key = f"{key}:inputs"
-    output_key = f"{key}:outputs"
-
-    input_history = cache._redis.lrange(input_key, 0, -1)
-    output_history = cache._redis.lrange(output_key, 0, -1)
-
-    print(f"{key} was called {len(input_history)} times:")
-    for inputs, output in zip(input_history, output_history):
-        inputs = eval(inputs.decode())
-        output = output.decode()
-        print(f"{key}{inputs} -> {output}")
+    def get_str(self, value: bytes) -> str:
+        return value.decode("utf-8")
+    def get_int(self, value: bytes) -> int:
+        return int.from_bytes(value, sys.byteorder)
