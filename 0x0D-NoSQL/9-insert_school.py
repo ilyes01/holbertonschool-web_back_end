777104c1f2e50545ca2952d34288a19ecd02625c
ilyes01 <ilyesghrab7@gmail.com>
2023-03-07 15:11:38 +0100

    insert

--- 0x0D-NoSQL/9-insert_school.py
+++ 0x0D-NoSQL/9-insert_school.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-"""python function"""
-from typing import Dict
-from pymongo.collection import Collection
+def insert_school(mongo_collection, **kwargs):
+    new_document = {}
+    for key, value in kwargs.items():
+        new_document[key] = value
 
+    result = mongo_collection.insert_one(new_document)
 
-def insert_school(mongo_collection: Collection, **kwargs):
-    """ function that insert dociment"""
-    result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
