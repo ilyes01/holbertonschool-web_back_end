5bcb427a5f36b84a911675152ece5372a513f051
ilyes01 <ilyesghrab7@gmail.com>
2023-03-07 13:49:37 +0100

    read and recover

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import uuid
 import redis
-from typing import Union
+from typing import Callable, Union
 
 class Cache:
     def __init__(self):
@@ -12,3 +12,17 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            data = fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        return self.get(key, fn=lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        return self.get(key, fn=lambda x: int(x))
