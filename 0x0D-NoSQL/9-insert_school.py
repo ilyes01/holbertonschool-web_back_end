34af5ac997bd2395915f1fc52e580067e13778a7
ilyes01 <ilyesghrab7@gmail.com>
2023-03-08 13:46:51 +0100

    insert

--- 0x0D-NoSQL/9-insert_school.py
+++ 0x0D-NoSQL/9-insert_school.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+""" module list"""
 def insert_school(mongo_collection, **kwargs):
      """documents of all list"""
-     return mongo_collection.insert_one(kwargs).inserted_id or Non
+     return mongo_collection.insert_one(kwargs).inserted_id or None
