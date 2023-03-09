92fca6acefeedd478c01db059865c384760f4fc2
ilyes01 <ilyesghrab7@gmail.com>
2023-03-09 16:57:54 +0100

    update

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,9 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter(student => student.city === city)
-    .map(student => {
-      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
-      return {
-        ...student,
-        grade: gradeObj ? gradeObj.grade : 'N/A'
-      };
-    });
+export default function getStudentIdsSum(students) {
+  let sum = 0;
+  if (Array.isArray(students)) {
+    for (let i = 0; i < students.length; i += 1) { // use i += 1 instead of i++
+      sum += students[i].id;
+    }
+  }
+  return sum;
 }
