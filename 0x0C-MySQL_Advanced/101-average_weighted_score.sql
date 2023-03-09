3b8a23d8c8bdb06c47be268210247d47fcbe71b5
ilyes01 <ilyesghrab7@gmail.com>
2023-03-09 13:24:55 +0100

    last updatee   i hope

--- 0x0C-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x0C-MySQL_Advanced/101-average_weighted_score.sql
@@ -4,12 +4,12 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DROP VIEW IF EXISTS users_stats;
-    CREATE VIEW users_stats AS
-    SELECT cor.user_id, (SUM(cor.score * pro.weight) / SUM(pro.weight)) as arg 
-    FROM corrections AS cor
-    JOIN projects AS pro ON pro.id = cor.project_id
-    GROUP BY cor.user_id;
+DROP TEMPORARY TABLE IF EXISTS users_stats;
+CREATE TEMPORARY TABLE users_stats AS
+SELECT cor.user_id, (SUM(cor.score * pro.weight) / SUM(pro.weight)) as weighted_score
+FROM corrections AS cor
+JOIN projects AS pro ON pro.id = cor.project_id
+GROUP BY cor.user_id;
 UPDATE users AS usr
 INNER JOIN users_stats uss ON usr.id = uss.user_id
 SET usr.average_score = uss.weighted_score;
