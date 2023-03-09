a3f2a9f9cd2628309fa32f4dba5e5259666a1bc6
ilyes01 <ilyesghrab7@gmail.com>
2023-03-09 13:35:25 +0100

    update

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,7 @@
---sql script that lists alll bands with glam rock 
---ranked by their longevity
-SELECT band_name, (IFNULL(split, 2020) - fomred) AS lifespan
-FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+--comments
+--comments
+SELECT band_name,
+    (YEAR(split) - YEAR(formed)) as lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
