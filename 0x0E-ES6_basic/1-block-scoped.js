48be6f09782aa9a60f2917b2ff57c0042088eede
ilyes01 <ilyesghrab7@gmail.com>
2023-03-14 10:53:52 +0100

    update to fix the eslint

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
-  if (trueOrFalse); {
+  if (trueOrFalse) {
     /* disable */
-    const task = true;
-    const task2 = false;
+    task = true;
+    task2 = false;
     /* enable */
   }
 
